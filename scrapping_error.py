6a7ba3e6f2b3c791af34addebee076ef7aae5e26
gleycealvess <gleyce.alves@industrycare.com.br>
2022-08-29 19:01:03 -0300

    add alterações

--- scrapping_error.py
+++ scrapping_error.py
@@ -1,49 +1,70 @@
 import re
 import os
 import csv
+import datetime
+import shutil, tempfile
 
 per_error={}
-directory = '/home/ubuntu/'
+directory = ['/home/ubuntu/mainservice/','/home/ubuntu/mailservice','/home/ubuntu/botservice','/home/ubuntu/reposervice','/home/ubuntu/simulationservice','/home/ubuntu/userauthservice_new']
+datatime = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
 
-for root, dirs, files in os.walk(directory):
-    for filename in files:
-        if re.search(r".*(?=log$)",filename):
-            file = os.path.join(root, filename)
-            print(file)
-            with open(file, 'r') as text_file:
-                    text_file=text_file.read()
-                    error_geral = re.findall(r'(?P<timestamp>)(?P<errortype>[a-z]+Error:)+(?P<description>[\s\S]+?(?=\b[a-z][)]|$))', text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE | re.MULTILINE)
-                    error_broken = re.findall (r'(?P<timestamp>[a-zA-z]+\s[a-zA-z]+\s\s\d\s\d{2}:\d{2}:\d{2}\s\d{4})\s-\suwsgi_response_write_headers_do\(\):\s(?P<errortype>Broken pipe)+(?P<description>[\s\S]+?(?=\b[a-z][)]|$))', text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE)
-                    error_server = re.findall(r'(?P<timestamp>)(?P<errortype>[a-z]+\s[a-z]+\sError:)+(?P<description>\s\/[a-z]+\/[a-z]+\/[a-z]+)',text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE)
+for x in range(len(directory)):
+    
+    for root, dirs, files in os.walk(directory[x]):
+        for filename in files:
+            if re.search(r".*(?=log$)",filename):
+                file = os.path.join(root, filename)
+                # print(file)
+                with open(file, 'r') as text_file:
+                        text_file=text_file.read()
+                        error_geral = re.findall(r'(?P<timestamp>)(?P<errortype>[a-z]+Error:)+(?P<description>[\s\S]+?(?=\b[a-z][)]|$))', text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE )
+                        error_broken = re.findall (r'(?P<timestamp>[a-zA-z]+\s[a-zA-z]+\s\s\d\s\d{2}:\d{2}:\d{2}\s\d{4})\s-\suwsgi_response_write_headers_do\(\):\s(?P<errortype>Broken pipe)+(?P<description>[\s\S]+?(?=\b[a-z][)]|$))', text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE)
+                        error_server = re.findall(r'(?P<timestamp>)(?P<errortype>[a-z]+\s[a-z]+\sError:)+(?P<description>\s\/[a-z]+\/[a-z]+\/[a-z]+)',text_file, re.IGNORECASE | re.UNICODE | re.MULTILINE)
+                        microservice = re.findall(r'(?P<microservice>(?s)(?s)(?<=\/ubuntu\/).*?(?=\/\b[a-z_]+\b))', file, re.IGNORECASE | re.UNICODE | re.MULTILINE )
+                        # print(microservice)
 
-                    for timestamp, errortype, description in error_geral:
-                        per_error[timestamp, errortype,description] = {
-                            "timestamp": None,
-                            "tipoErro": errortype,
-                            "descricao": description    
-                        }
+                        for microservice in microservice:
+                            for timestamp, errortype, description in error_geral:
+                                per_error[ timestamp, errortype,description, microservice] = {
+                                "microserviço": microservice,
+                                "timestamp": datatime,
+                                "tipoErro": errortype,
+                                "descricao": description
+                                
+                            }
 
-                    for timestamp, errortype, description in error_server:
-                        per_error[timestamp, errortype,description] = {
-                            "timestamp": None,
-                            "tipoErro": errortype,
-                            "descricao": description
-                        }
-                        
-                    for timestamp, errortype, description in error_broken:
-                        per_error[timestamp, errortype, description] = {
-                            "timestamp": timestamp,
-                            "tipoErro": errortype,
-                            "descricao": description
-                        }
+                            for timestamp, errortype, description in error_server:
+                                per_error[ timestamp, errortype,description, microservice] = {
+                                "microserviço": microservice,
+                                "timestamp": datatime,
+                                "tipoErro": errortype,
+                                "descricao": description
+                                }
+                                    
+                            for timestamp, errortype, description in error_broken:
+                                per_error[ timestamp, errortype, description, microservice] = {
+                                "microserviço": microservice,
+                                "timestamp": datatime,
+                                "tipoErro": errortype,
+                                "descricao": description
+                                } 
 
-                   
+        #apenas para debbuging   
         for error_data in per_error.values():
             print(error_data) 
 
+        #adiciona os erros encontrados no arquivo csv
         with open('errorlog.csv', 'a') as csvfile:
-            fieldnames = [ "timestamp", "tipoErro", "descricao"]
+            fieldnames = [ "microserviço", "timestamp", "tipoErro", "descricao"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader() 
             for error_data in per_error.values():
                 writer.writerow(error_data)   
+
+        # move todo o conteúdo do "uwsgi_error.log" para o arquivo "uwsgi_error_permanente.log"
+        if re.search(r"uwsgi_error.log",filename):  
+            with open("uwsgi_error.log") as temporario:
+                with open("uwsgi_error_permantente.log", "a") as permanente:
+                    for line in temporario:
+                        permanente.write(line)  
+
